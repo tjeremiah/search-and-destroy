5b60a192ab025ad5f0123b156fd175f97f1d121a
HopeFourie <hopefourie@gmail.com>
2020-10-28 16:13:20 -0400

    binary search and looper functions written

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,20 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  let nexts = [];
+  let currentNode = linkedlist.head;
+  do {
+    if (currentNode.next || nexts.length > 0) {
+      if (nexts.includes(currentNode.next)) {
+        return true;
+      } else {
+        nexts.push(currentNode.next);
+      }
+    }
+    currentNode = currentNode.next;
+  } while (currentNode);
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
